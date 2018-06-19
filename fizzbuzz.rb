d351634887f7a171e97601b27dcc9a43abc497d4
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-19 17:19:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,14 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
    "Fizzbuzz"
-   
+
   elsif int % 3 == 0
     "Fizz"
-    
+
   elsif
-    int % 5 == 0 
+    int % 5 == 0
     "Buzz"
   else
     "nil"
   end
 end
-
-puts fizzbuzz(15)
-puts fizzbuzz(33)
-puts fizzbuzz(30)
